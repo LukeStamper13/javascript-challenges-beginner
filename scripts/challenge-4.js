d622d9f294dedde58a61cd7eeb9a9bdfaf5dbf72
Luke Stamper <luke.stamper12@outlook.com>
2022-10-06 16:20:37 -0600

    started 4

--- scripts/challenge-4.js
+++ scripts/challenge-4.js
@@ -20,3 +20,21 @@ if (!output) throw "Output container not found.";
 
 //@ts-check
 // your code starts after this line
+
+let negAndPos = [
+	-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+];
+
+let intAndDec = [
+	-10.9,
+	-10.8,
+	-10.7,
+	-10.6,
+	-10.5,
+	-10.4,
+	-10.3,
+	-10.2,
+	-10.1,
+	-10,
+	
+];
